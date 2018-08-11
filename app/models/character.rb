bf93237011b0a6551fb4d0847730e8699c820104
Nathan  Visak <nathan.visak@gmail.com>
2018-08-11 19:39:42 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,8 +6,8 @@ class Character < ActiveRecord::Base
     "#{self.name} always says: #{self.catchphrase}"
   end
 
-  def build_show(:name)
-    name: self.show.name
+  def build_show(name)
+    name = self.show.name
   end
 
 
