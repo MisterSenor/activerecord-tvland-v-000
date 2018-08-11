9957f436f4379ad40bd0259efd9a1a6e80e14a1b
Nathan  Visak <nathan.visak@gmail.com>
2018-08-11 19:56:32 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,17 +1,11 @@
 class Show < ActiveRecord::Base
   has_many :characters
-  has_many :actors, through: :characters 
-  belongs_to :network 
+  has_many :actors, through: :characters
+  belongs_to :network
 
   def build_network(call_letters)
     new_network = Network.create(call_letters)
-    
-    self.character.build_show
-    # def build_show(name)
-    #   #name of the show = parameter
-    #   new_show = Show.create(name)
-    #   self.show = new_show 
-    # end
-  end 
-  
+    self.network = new_network
+  end
+
 end
